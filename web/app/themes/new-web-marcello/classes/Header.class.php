7d4a8d1d5e3a74296c69527a319b538f720d77fe
webmarcello8080 <webmarcello8080@gmail.com>
2021-02-14 16:43:31 +0000

    cleaning metatag in header and extra CSS in header

--- web/app/themes/new-web-marcello/classes/Header.class.php
+++ web/app/themes/new-web-marcello/classes/Header.class.php
@@ -14,5 +14,13 @@ class Header{
 
       // remove meta genarator
       remove_action('wp_head', 'wp_generator');
+
+      add_action('wp_enqueue_scripts', array( $this, 'load_assets'));
+   }
+
+   public function load_assets(){
+      wp_enqueue_style( 'new_web_marcello-stylesheet', get_template_directory_uri() . '/dist/css/styles.css', array(), '1.0.0', 'all' );
+
+      wp_enqueue_script( 'new_web_marcello-scripts', get_template_directory_uri() . '/dist/js/scripts.js', array('jquery'), '1.0.0', true );
    }
 }
--- web/app/themes/new-web-marcello/functions.php
+++ web/app/themes/new-web-marcello/functions.php
@@ -8,10 +8,3 @@ foreach (glob( get_template_directory() . "/classes/*.php") as $filename)
 
 new \NewWebMarcello\CustomTheme();
 new \NewWebMarcello\Header();
-
-function new_web_marcello_assets() {
-  wp_enqueue_style( 'new_web_marcello-stylesheet', get_template_directory_uri() . '/dist/css/styles.css', array(), '1.0.0', 'all' );
-
-  wp_enqueue_script( 'new_web_marcello-scripts', get_template_directory_uri() . '/dist/js/scripts.js', array('jquery'), '1.0.0', true );
-}
-add_action('wp_enqueue_scripts', 'new_web_marcello_assets');
