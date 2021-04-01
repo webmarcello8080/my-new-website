13ec85671f302154408dd9a88e7da126d97c5b58
Marcello Perri <webmarcello8080@gmail.com>
2021-04-01 19:56:31 +0100

    header on blog page

--- web/app/themes/new-web-marcello/template-parts/header/category-header.php
+++ web/app/themes/new-web-marcello/template-parts/header/category-header.php
@@ -6,7 +6,7 @@ $mid_square_background = file_get_contents( get_template_directory() . "/dist/im
 <header class="category-header bg-light">
    <div class="container-xl py-5 category-header-wrapper">
       <h1 class="category-title mb-5"><?= single_cat_title() ?></h1>
-      <h5 class="category-description"><?= category_description() ?></h5>
+      <?php the_archive_description('<div class="category-description lead">', '</div>') ?>
    </div>
    <div class="mid-square-background img-fluid"><?= $mid_square_background ?></div>
    <div class="smallest-square-background"><?= $smallest_square_background ?></div>
