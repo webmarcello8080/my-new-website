e6ab0d2bd3d5dcfbb345c5a8f16be492bb9d9187
Marcello Perri <webmarcello8080@gmail.com>
2021-04-01 12:53:52 +0100

    blog block and block template improved

--- web/app/themes/new-web-marcello/template-parts/loop/blog.php
+++ web/app/themes/new-web-marcello/template-parts/loop/blog.php
@@ -1,27 +1,27 @@
 <?php 
 $categories = new \NewWebMarcello\Categories; 
-if(has_post_thumbnail()){
-   $blog_header_class =  'blog-header-aside';
-} else {
-   $blog_header_class =  'blog-header';
-}
+// if(has_post_thumbnail()){
+//    $blog_header_class =  'blog-header-aside';
+// } else {
+//    $blog_header_class =  'blog-header';
+// }
 ?>
 <div class="col-md-4">
    <div class="card border-0 rounded-lg my-3 text-left single-blog">
-      <div class="card-img-top blog-img">
-         <a href="<?php the_permalink() ?>" ><?php the_post_thumbnail( 'large', ['class' => 'img-fluid'] ); ?></a>
+      <div class="card-img-top p-3 blog-img">
+         <a href="<?php the_permalink() ?>" ><?php the_post_thumbnail( 'large', ['class' => 'img-fluid rounded'] ); ?></a>
       </div>
-      <div class="card-body <?= $blog_header_class ?>">
-         <div class="d-flex justify-content-between mb-3">
-            <div class="blog-author"><?= get_the_author_meta('display_name'); ?></div>
-            <div class="blog-date"><?= get_the_date(); ?></div>
-         </div>
-         <h6 class="card-title blog-title mb-1"><a href="<?php the_permalink() ?>" ><?php the_title(); ?></a></h6>
+      <div class="card-body blog-header">
+         <h6 class="blog-author"><?= get_the_author_meta('display_name'); ?></h6>
+         <h6 class="card-title mb-2 blog-title"><a href="<?php the_permalink() ?>" ><?php the_title(); ?></a></h6>
       </div>
-      <div class="card-body pt-2 blog-body">
+      <div class="card-body pt-0 blog-body">
          <div class="blog-categories"><?php $categories->getCategoryList(get_the_ID(), 'badge badge-primary', ' ') ?></div>
-         <p class="card-text blog-text mt-3"><?= get_the_excerpt() ?></p>
-         <h6 class="blog-read-more"><a href="<?php the_permalink() ?>" >Read More ...</a></h6>
+         <p class="card-text blog-text mt-3"><small><?= get_the_excerpt() ?></small></p>
+         <div class="d-flex justify-content-between">
+            <div class="blog-date"><?= get_the_date(); ?></div>
+            <h6 class="blog-read-more"><a href="<?php the_permalink() ?>" >Read More ...</a></h6>
+         </div>
       </div>
    </div>
 </div>
