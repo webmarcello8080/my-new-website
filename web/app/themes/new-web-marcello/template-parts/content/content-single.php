4fd055974a7433f3ac50580aaf5b55efef23a784
Marcello Perri <webmarcello8080@gmail.com>
2021-04-03 17:39:01 +0100

    page and category header like the hero section

--- web/app/themes/new-web-marcello/template-parts/content/content-single.php
+++ web/app/themes/new-web-marcello/template-parts/content/content-single.php
@@ -1,4 +1,5 @@
 <?php
+$categories = new \NewWebMarcello\Categories; 
 $small_horizontal_line = file_get_contents( get_template_directory() . "/dist/images/small-horizontal-line.svg");
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -10,6 +11,7 @@ $small_horizontal_line = file_get_contents( get_template_directory() . "/dist/im
 	<?php endif; ?>
 
 	<div class="post-entry-content">
+		<div class="post-categories mb-5"><?php $categories->getCategoryList(get_the_ID(), 'badge badge-pill badge-primary', ' ') ?></div>
 	   <h2 class="post-description text-center mb-2"><?= get_post_meta(get_the_ID(), 'H2_text', TRUE); ?></h2>
 		<div class="small-horizontal-line text-center mb-5"><?= $small_horizontal_line ?></div>
 		<?php
--- web/app/themes/new-web-marcello/template-parts/header/category-header.php
+++ web/app/themes/new-web-marcello/template-parts/header/category-header.php
@@ -1,13 +1,13 @@
 <?php
 // get background images
+$hero_background = file_get_contents( get_template_directory() . "/dist/images/hero-background.svg");
 $smallest_square_background = file_get_contents( get_template_directory() . "/dist/images/smallest-square-background.svg");
-$mid_square_background = file_get_contents( get_template_directory() . "/dist/images/mid-square-background.svg");
 ?>
-<header class="category-header bg-light">
+<header class="category-header">
    <div class="container-xl py-5 category-header-wrapper">
-      <h1 class="category-title mb-5"><?= single_cat_title() ?></h1>
+      <h1 class="category-title"><?= single_cat_title() ?></h1>
       <?php the_archive_description('<div class="category-description lead">', '</div>') ?>
    </div>
-   <div class="mid-square-background img-fluid"><?= $mid_square_background ?></div>
    <div class="smallest-square-background"><?= $smallest_square_background ?></div>
+   <div class="hero-background-image"><?= $hero_background ?></div>
 </header>
