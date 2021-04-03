0b5653523579723f5ceccb3bdf60b5916b5d97d7
Marcello Perri <webmarcello8080@gmail.com>
2021-04-03 17:58:25 +0100

    page and category header like the hero section

--- web/app/themes/new-web-marcello/template-parts/header/page-header.php
+++ web/app/themes/new-web-marcello/template-parts/header/page-header.php
@@ -1,13 +1,11 @@
 <?php
 $featured_image = get_the_post_thumbnail_url();
 // get background images
-$smallest_square_background = file_get_contents( get_template_directory() . "/dist/images/smallest-square-background.svg");
-$mid_square_background = file_get_contents( get_template_directory() . "/dist/images/mid-square-background.svg");
+$hero_background = file_get_contents( get_template_directory() . "/dist/images/hero-background.svg");
 ?>
 <header class="page-header mb-5" style="background-image: url('<?php echo $featured_image; ?>')">
    <div class="container-xl py-5 page-header-wrapper">
       <h1 class="page-title"><?= get_the_title() ?></h1>
    </div>
-   <div class="mid-square-background img-fluid"><?= $mid_square_background ?></div>
-   <div class="smallest-square-background"><?= $smallest_square_background ?></div>
+   <div class="hero-background-image"><?= $hero_background ?></div>
 </header>
