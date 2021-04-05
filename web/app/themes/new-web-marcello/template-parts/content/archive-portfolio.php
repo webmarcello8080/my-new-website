d97aa0ff6efc73968851e105618a7b7c9a9bdaaa
Marcello Perri <webmarcello8080@gmail.com>
2021-04-05 15:31:27 +0100

    portfolio archive and new category class methods

--- web/app/themes/new-web-marcello/template-parts/content/archive-portfolio.php
+++ web/app/themes/new-web-marcello/template-parts/content/archive-portfolio.php
@@ -17,16 +17,6 @@ $no_posts_text_1 = $new_web_marcello_theme_options['no_posts_text_1'];
 			<?php wp_reset_postdata(); ?>
 
 		</div>
-
-		<!-- <?php the_posts_pagination( 
-					array( 
-						'screen_reader_text' => ' ', 
-						'mid_size' => 2,
-						'prev_text' => '&#x300a',
-						'next_text' => '&#x300b'
-					) 
-				);
-		?> -->
 		
 	<?php else : ?>
 		<p><?= esc_attr($no_posts_text_1) ?></p>
