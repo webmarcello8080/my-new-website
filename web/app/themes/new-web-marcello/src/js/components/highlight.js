273bd87d0a992f0fe35b99675d56247386f3ccf0
Marcello Perri <webmarcello8080@gmail.com>
2021-04-08 13:49:14 +0100

    highlight pre html tags

--- web/app/themes/new-web-marcello/src/js/components/highlight.js
+++ web/app/themes/new-web-marcello/src/js/components/highlight.js
@@ -1,4 +1,7 @@
 import hljs from 'highlight.js';
+import php from 'highlight.js/lib/languages/php';
+hljs.registerLanguage('php', php);
+
 document.querySelectorAll('pre.code-snipping').forEach(block => {
    // then highlight each
    hljs.highlightBlock(block);
