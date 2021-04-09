708b6fb7549ebe7af6d52bc4fe0416f577f2f339
Marcello Perri <webmarcello8080@gmail.com>
2021-04-09 21:50:01 +0100

    google analytics

--- web/app/themes/new-web-marcello/header.php
+++ web/app/themes/new-web-marcello/header.php
@@ -5,6 +5,15 @@
 <!--[if IE 8]>    <html class="no-js ie8 oldie" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-3E856EGMN6"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-3E856EGMN6');
+	</script>
    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
