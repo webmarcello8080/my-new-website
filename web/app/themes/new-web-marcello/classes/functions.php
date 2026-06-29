e1f820e9fb23dc96cb5c4a2e38667a258a1ca43f
Marcello Perri <webmarcello8080@gmail.com>
2026-06-29 13:22:25 +0100

    remove polyfill

--- web/app/themes/new-web-marcello/classes/functions.php
+++ web/app/themes/new-web-marcello/classes/functions.php
@@ -5,3 +5,10 @@ function cc_mime_types($mimes) {
     return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/**
+ * Disables the polyfill.io script in Site Reviews.
+ * Paste this in your active theme's functions.php file.
+ * @return bool
+ */
+add_filter('site-reviews/assets/polyfill', '__return_false');
